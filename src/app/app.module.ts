b0e084a21645f743a61bc2e7c51cbcf0ecfcb736
DESKTOP-IDBPR9R\MrGadget <diastra777@gmail.com>
2017-09-16 12:28:00 +0300

    Add Angular Material and Angular CDK

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MdButtonModule, MdCheckboxModule} from '@angular/material';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,BrowserAnimationsModule,MdButtonModule, MdCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
